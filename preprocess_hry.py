217ec4dcbc2fbe6c05beec31cecbc4ebfe4513ad
HariyaNobuki <HariyaNobuki@users.noreply.github.com>
2022-06-09 03:57:19 +0900

    sanaだよね

--- preprocess_hry.py
+++ preprocess_hry.py
@@ -1,30 +1,26 @@
-# love sana
+# love sana twice
 import os
 import sys
 
 # add get module
 import os , sys
-from glob import glob
-from PIL import Image
-
 from keras.preprocessing.image import load_img, save_img, img_to_array, array_to_img
 
 
-def resize_images(image_path, image_save_dir, image_size):
-    img = load_img(image_path, grayscale=False, color_mode='rgb', target_size=(224,224))
-    a=0
-
-
-
-
 def _main():
-    images_dir = 'sign/sana.png'  # input directory
+    images_dir = 'sign/taisei.bmp'  # input directory
     image_save_dir = 'resize_image/'  # output directory
-    image_size = 100                    # ここはどうにか後で変えようかな？
+    image_size = 224                    # ここはどうにか後で変えようかな？
     if len(sys.argv) > 1:
         image_size = int(sys.argv[1])
+    
+    img = load_img(images_dir, grayscale=False, color_mode='rgb', target_size=(224,224))
+    iomaray = img_to_array(img)
+    #an_pic = array_to_img(iomaray, scale=True)
+    save_img("nsana.jpg", iomaray)
+    a=0
 
-    resize_images(images_dir=images_dir, image_save_dir=image_save_dir, image_size=image_size)
+    #resize_images_hry(images_dir=images_dir,image_save_dir=image_save_dir, image_size=image_size)
 
 
 if __name__ == '__main__':
