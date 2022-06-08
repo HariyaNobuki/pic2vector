6d69764578fb29239ce46bb2b828e6efbeaf7876
HariyaNobuki <HariyaNobuki@users.noreply.github.com>
2022-06-09 04:16:24 +0900

    慣性

--- preprocess_hry.py
+++ preprocess_hry.py
@@ -1,4 +1,5 @@
 # love sana twice
+# @ HRY CONSULTING
 import os
 import sys
 import cv2
@@ -6,26 +7,22 @@ import cv2
 # add get module
 from keras.preprocessing.image import load_img, save_img, img_to_array, array_to_img
 
-target_size = [112,224]
+target_size = [112,224,248,1404]
 images_dir = 'sign/taisei.bmp'  # input directory
 image_save_dir = 'resize_image/'  # output directory
-os.makedirs(image_save_dir,exist_ok=True
-image_size = 224                    # ここはどうにか後で変えようかな？
+os.makedirs(image_save_dir,exist_ok=True)
+
 if len(sys.argv) > 1:
     image_size = int(sys.argv[1])
 
 def _main(dpi):
     im = cv2.imread('sign/taisei.bmp')  # <class 'numpy.ndarray'>
-    print(im.shape)     # <tuple>
+    #print(im.shape)     # <tuple>
+    assert dpi < im.shape[0] , "you choice too big dpi for comparing original pic size (%d->%d)"%(dpi , im.shape[0])
     aspect = im.shape[1] / im.shape[0]
-    
     img = load_img(images_dir, grayscale=False, color_mode='rgb', target_size=(int(dpi),int(dpi*aspect)))
     i2ary = img_to_array(img)
-    save_img(image_save_dir+ "{}.jpg".format(dpi), i2ary)
-    a=0
-
-    #resize_images_hry(images_dir=images_dir,image_save_dir=image_save_dir, image_size=image_size)
-
+    save_img(image_save_dir+ "{}.bmp".format(dpi), i2ary)
 
 if __name__ == '__main__':
     for dpi in target_size:
